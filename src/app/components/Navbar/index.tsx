57fd3e6db3ebbc9205caf0a710be43ff23a58d65
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-10-02 16:07:37 +0100

    Responsive navbar
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -10,20 +10,31 @@ export default function Navbar() {
   const pathName = usePathname();
   return (
     <nav className="grid w-full fixed top-0 md:py-2 z-[500]">
-      <div className="w-full xl:w-1/3 mx-auto md:w-2/3 text-xs text-white md:rounded-full py-2 px-14 items-center backdrop-blur-xl bg-zinc-700 bg-opacity-[0.35] flex flex-row justify-between">
-        <Image src={logo} alt="York Engineering Society Logo" className="hidden md:inline-block"/>
-        <Link href="/" className={pathName.length === 1 ? "active" : ""}>
+      <div className="w-full xl:w-1/3 mx-auto md:w-2/3 text-xs text-white md:rounded-full py-2 px-14 backdrop-blur-xl bg-zinc-700 bg-opacity-[0.35] grid grid-rows-1 grid-cols-3 md:grid-cols-4 items-center">
+        <Image
+          src={logo}
+          alt="York Engineering Society Logo"
+          className="hidden md:inline-block p-2 mx-auto"
+        />
+        <Link
+          href="/"
+          className={`w-max mx-auto ${pathName.length === 1 ? "active" : ""}`}
+        >
           Home
         </Link>
         <Link
           href="/about"
-          className={pathName.includes("about") ? "active" : ""}
+          className={`w-max mx-auto ${
+            pathName.includes("about") ? "active" : ""
+          }`}
         >
           About
         </Link>
         <Link
           href="/events"
-          className={pathName.includes("events") ? "active" : ""}
+          className={`w-max mx-auto ${
+            pathName.includes("events") ? "active" : ""
+          }`}
         >
           Events
         </Link>
