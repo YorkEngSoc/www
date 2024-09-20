825e5c935f84153892d5bdab3a6033e8ffccba2e
Harry Boyd <hboyd255@gmail.com>
2024-09-20 14:08:09 +0100

    Updated link to calendar

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
       {
         source: "/calendar",
         destination:
-          "https://calendar.google.com/calendar/u/5?cid=Y181YzM0ZGYwMDUyMWE5MWQ3ZjJlYjgwMWQ2MTRlYmUyODM1YTE4NmM4YTE3ZmVkZTZhZDMyOTliMGU1ZTAyZDI1QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20",
+          "https://calendar.google.com/calendar/embed?src=c_5c34df00521a91d7f2eb801d614ebe2835a186c8a17fede6ad3299b0e5e02d25%40group.calendar.google.com&ctz=Europe%2FLondon&mode=AGENDA&hl=en_GB&showNav=0&showDate=0&showPrint=0&showTabs=0&showTz=0&showCalendars=0&color=%233F51B5&bgcolor=%233F51B5",
         basePath: false,
         permanent: true,
       },
